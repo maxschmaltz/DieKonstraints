3fa621714b7d5968458158333965251f34839c65
Maksim Shmalts <maksim.shmalts@uni-tuebingen.de>
2026-01-15 16:51:38 +0100

    update applicability checks

--- compounding/constraints/applicability_check.py
+++ compounding/constraints/applicability_check.py
@@ -1,14 +1,19 @@
-from compounding.constraints.data_utils.gecodb_compound_parser import Compound
+from compounding.constraints.data_utils.gecodb_compound_parser import Compound, Linker
 
 
-# 				zero_default
+	# # here and further: since we focus on N+N
+	# # compounds, only one linker is always expected
+	# linker: Linker = compound.links[0]
+	# return linker.allomorph == "" and not linker.adds_umlaut
+
+
+# 				def-0
 # The majority of compounds have a zero linker.
+# Zero linker is default for German compounds.
 
 def zero_default_is_applicable(compound: Compound):
 	# applicable by default
 	return True
 
 def zero_default_applies(compound: Compound):
-	# here and further: since we focus on N+N
-	# compounds, only one link is always expected
-	return compound.links[0].type == "zero"
+	pass
