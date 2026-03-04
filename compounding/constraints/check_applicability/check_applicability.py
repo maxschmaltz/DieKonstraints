296917f51a62cb2bf40d0bbd48b5dacfb6332ab8
Maksim Shmalts <maksim.shmalts@uni-tuebingen.de>
2026-03-04 14:22:24 +0100

    remove unzipped file

--- compounding/constraints/check_applicability/check_applicability.py
+++ compounding/constraints/check_applicability/check_applicability.py
@@ -181,6 +181,7 @@ def main():
     zip_name = os.path.splitext(appl_index_path)[0] + ".zip"
     with zipfile.ZipFile(zip_name, "w", compression=zipfile.ZIP_DEFLATED) as z:
         z.write(appl_index_path, os.path.basename(appl_index_path))
+    os.remove(appl_index_path)
         
     constr_statistics.to_csv(
         os.path.join(outpath, "constr_statistics.tsv"),
